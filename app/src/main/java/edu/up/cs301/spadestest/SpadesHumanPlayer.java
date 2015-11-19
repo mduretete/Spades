969c37d6acbf3848eb52bdee5a455f83dbc9aef2
mduretete <duretete16@up.edu>
2015-11-18 18:43:49 -0800

    SpadesHumanPlayer beginnings

--- app/src/main/java/edu/up/cs301/spadestest/SpadesHumanPlayer.java
+++ app/src/main/java/edu/up/cs301/spadestest/SpadesHumanPlayer.java
@@ -2,10 +2,16 @@ package edu.up.cs301.spadestest;
 
 import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Space;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import edu.up.cs301.game.GameHumanPlayer;
 import edu.up.cs301.game.GameMainActivity;
 import edu.up.cs301.game.infoMsg.GameInfo;
+import edu.up.cs301.game.infoMsg.GameState;
 
 /**
  * @author Ryan Morrison, Jin Mok, Nick Wagner, Maddy Duretete
@@ -16,6 +22,34 @@ import edu.up.cs301.game.infoMsg.GameInfo;
  *      holds
  */
 public class SpadesHumanPlayer extends GameHumanPlayer{
+
+    // Widgets to be used and modified during play
+    private TextView playerScoreTextView;
+    private TextView partnerScoreTextView;
+    private TextView LScoreTextView;
+    private TextView RScoreTextView;
+    private TextView playerTrickTextView;
+    private TextView partnerTrickTextView;
+    private TextView LTrickTextView;
+    private TextView RTrickTextView;
+    private Button card0;
+    private Button card1;
+    private Button card2;
+    private Button card3;
+    private Button card4;
+    private Button card5;
+    private Button card6;
+    private Button card7;
+    private Button card8;
+    private Button card9;
+    private Button card10;
+    private Button card11;
+    private Button card12;
+    private Space dropSpace;
+
+    private GameMainActivity myActivity;
+
+
 
     /**
      * SpadesHumanPlayer():ctor for the human player
@@ -31,7 +65,7 @@ public class SpadesHumanPlayer extends GameHumanPlayer{
      */
     @Override
     public View getTopView() {
-        return null;
+        return myActivity.findViewById(R.id.top_gui_layout);
     }
 
     /**
@@ -39,7 +73,10 @@ public class SpadesHumanPlayer extends GameHumanPlayer{
      * @param info
      */
     public void receiveInfo(GameInfo info) {
-
+        if (info instanceof SpadesState) {
+            SpadesState myGameState = (SpadesState) info;
+            playerScoreTextView.setText("" + myGameState.getPlayerScore(0) );
+        }
     }
 
     /**
