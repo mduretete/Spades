8655880fa4ea7c1860c8e67831d4b70392c762a7
Maddy Duretete <duretete16@up.edu>
2015-12-11 07:53:51 +0100

    Unbroken, Nick's commit added

--- app/src/main/java/edu/up/cs301/spadestest/SpadesLocalGame.java
+++ app/src/main/java/edu/up/cs301/spadestest/SpadesLocalGame.java
@@ -55,6 +55,11 @@ public class SpadesLocalGame extends LocalGame {
         } else if (spadesGameState.getTeam2Score() >= 200) {
             return "TEAM 2 WINS";
         }
+        else if (spadesGameState.getTeam1Score() <= -200){
+            return "TEAM 2 WINS";
+        } else if (spadesGameState.getTeam2Score() <= -200) {
+            return "TEAM 1 WINS";
+        }
         return null;
     }//checkIfGameOver()
 
